3dfa0665853bcf8f60feb8173b14136a597e588c
ZackZeta <107325334+ZackZeta@users.noreply.github.com>
2023-02-16 19:56:45 -0500

    updated sections for testing

--- client.py
+++ client.py
@@ -2,5 +2,20 @@
 
 import sys
 
-if __name__ == '__main__':
-    sys.stderr.write("client is not implemented yet\n")
+#importing socket
+import socket
+
+#if __name__ == '__main__':
+#    sys.stderr.write("server is not implemented yet\n")
+
+#creating new socket using socket method
+#socket.AF_INET for the address and protocol family for IPv4
+#socket.SOCK_STREAM Stream socket type, provides dual directional communication
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(sock)
+
+sock.bind(("localhost", 12345))
+print(sock)
+
+socket.listen(1)
+clientSocket, ClientAddress = sock.accept()
