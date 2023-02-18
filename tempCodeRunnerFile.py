83e06b5133b05c50b33d6480eaabc762849533d8
ZackZeta <107325334+ZackZeta@users.noreply.github.com>
2023-02-17 19:22:32 -0500

    working client server

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -6,23 +6,16 @@ import sys
 import socket
 
 #if __name__ == '__main__':
-#    sys.stderr.write("client is not implemented yet\n")
-    
-    
+#    sys.stderr.write("server is not implemented yet\n")
+
 #creating new socket using socket method
 #socket.AF_INET for the address and protocol family for IPv4
 #socket.SOCK_STREAM Stream socket type, provides dual directional communication
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(sock)
 
-#remote connect to ((hostname, port))
-sock.connect(("127.0.0.1", 65432))
+sock.bind(("localhost", 12345))
+print(sock)
 
-#sending initial buffer
-#b symbol buffer
-#ASCII control characters:(\r) Carriage Return and (\n) Line Feed.
-#used in many protocols to separate header and body fields, when text is sent between systems.
-l = sock.send(b"foobar\r\n")
-print("send bytes", l)
-
-b = sock.recv(1024)
-print("Received: '%s'" % b)
+socket.listen(1)
+clientSocket, ClientAddress = sock.accept()
