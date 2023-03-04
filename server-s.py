720e71516ff162d3cde26789b838caba14b28f1b
ZackZeta <107325334+ZackZeta@users.noreply.github.com>
2023-03-04 17:42:28 -0500

    filename

--- server-s.py
+++ server-s.py
@@ -40,7 +40,11 @@ def processClientConnection(conn, addr):
                 conn.close()
                 return
             filename = filename_line[1]
-            filesize = int(lines[1].split(b' ')[1])
+            if b'filesize=' not in header:
+                filesize = 0 # or some other default value
+                conn.send(b"Warning: file size not specified in header. Using default file size.\r\n")
+else:
+    filesize = int(lines[1].split(b' ')[1])
 
     # If header is empty or incomplete, send an error response and close the connection
     if not header or b'filename=' not in header or b'filesize=' not in header:
